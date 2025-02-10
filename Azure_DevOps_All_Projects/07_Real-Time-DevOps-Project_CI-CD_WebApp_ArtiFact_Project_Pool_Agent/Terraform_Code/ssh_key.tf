4ac048a14d65155248c27cc5e70c81900b6ea03d
Balraj Singh <53593772+mrbalraj007@users.noreply.github.com>
2025-02-10 14:33:30 +1100

    Delete Azure_DevOps_All_Projects/07_Real-Time-DevOps-Project_CI-CD_WebApp_ArtiFact_Project_Pool_Agent directory

--- Azure_DevOps_All_Projects/07_Real-Time-DevOps-Project_CI-CD_WebApp_ArtiFact_Project_Pool_Agent/Terraform_Code/ssh_key.tf
+++
@@ -1,9 +0,0 @@
-resource "local_file" "private_key" {
-  content  = tls_private_key.ssh_key.private_key_pem
-  filename = "${path.module}/id_rsa"
-}
-
-resource "local_file" "public_key" {
-  content  = tls_private_key.ssh_key.public_key_openssh
-  filename = "${path.module}/id_rsa.pub"
-}
